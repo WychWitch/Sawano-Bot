a8ad922b2367f4c845cfdf2fa5eca47dcac6bc83
Andy Ta <andyta97@ou.edu>
2020-12-29 18:40:37 -0600

    deserialized horse json in HorseModule

---
+++ PartyBot/Modules/HorseModule.cs
@@ -0,0 +1,21 @@
+﻿using Discord.Commands;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace PartyBot.Modules
+{
+    public class HorseModule : ModuleBase<SocketCommandContext>
+    {
+        public void LoadJson()
+        {
+            using (StreamReader reader = new StreamReader("horse.json"))
+            {
+                string json = reader.ReadToEnd();
+                Dictionary<string, string> horseDictionary = JsonConvert.DeserializeObject<Dictionary<string, string>>(json);
+            }
+        }
+    }
+}
