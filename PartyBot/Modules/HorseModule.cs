b46c59cce583445f91d6e83cfd86b76cd49a9776
retrosplicer <retrosplicer@gmail.com>
2020-12-30 18:49:08 -0600

    horse testing

--- PartyBot/Modules/HorseModule.cs
+++ PartyBot/Modules/HorseModule.cs
@@ -4,14 +4,24 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Text;
+using System.Threading.Tasks;
+using Discord;
+using Discord.WebSocket;
+using PartyBot.Services;
 
 namespace PartyBot.Modules
 {
     public class HorseModule : ModuleBase<SocketCommandContext>
     {
+        [Command("Horse")]
+
+       /* public async Task Horse()
+        {
+            return await 
+        } */
         public void LoadJson()
         {
-            using (StreamReader reader = new StreamReader("horse.json"))
+            using (StreamReader reader = new StreamReader(".../horse.json"))
             {
                 string json = reader.ReadToEnd();
                 Dictionary<string, string> horseDictionary = JsonConvert.DeserializeObject<Dictionary<string, string>>(json);
